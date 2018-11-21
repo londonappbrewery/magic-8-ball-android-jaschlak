a9eea9f5e98a73ed57ad4195e8ba59cc3d73ee06
jaschlak <jaschlak@gmail.com>
2018-11-20 20:40:06 -0600

    Finished app, modified height to fit my phone and moved the text into separate text views because text wrapping was occurring.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,12 +2,45 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity
+{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ImageView ballDisplay = (ImageView) findViewById(R.id.image_eightball);
+
+        final int[] diceArray = {R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5};
+
+        Button myButton;
+        myButton = (Button) findViewById(R.id.askButton);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("8-Ball", "Ze Button has been pressed!");
+
+                Random randomNumberGenerator = new Random();
+
+                int number = randomNumberGenerator.nextInt(5);
+                Log.d("Dicee", "The number is " + number);
+
+                ballDisplay.setImageResource(diceArray[number]);
+
+            }
+        });
     }
 }
